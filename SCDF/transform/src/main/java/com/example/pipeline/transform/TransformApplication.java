073fc9b9246f6a40cba113e8f3c86d113f333576
Cahlen Humphreys <cahlen@gmail.com>
2018-10-29 01:16:56 -0600

    Add initial spring gradle skeleton for transform
    This is the vanilla Spring Initializr out put for a project with SCS,
    Kafka integration, and Configuration processor.

---
+++ SCDF/transform/src/main/java/com/example/pipeline/transform/TransformApplication.java
@@ -0,0 +1,12 @@
+package com.example.pipeline.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransformApplication.class, args);
+    }
+}
