29874d1fa9882fcdd6cbd6c22c91dda91dbc5be5
Cahlen Humphreys <cahlen@gmail.com>
2018-10-29 05:54:45 -0600

    Add initial gradle skeleton for sink app
    This is the original untouched project structure taken from the Spring
    Initializr. Unique to this application is our use of mongo and spring
    data.

---
+++ SCDF/sink/src/main/java/com/example/pipeline/sink/SinkApplication.java
@@ -0,0 +1,12 @@
+package com.example.pipeline.sink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinkApplication.class, args);
+    }
+}
