e45a29402832cc699f96734d5ea3c018ed874c9b
Cahlen Humphreys <cahlen@gmail.com>
2018-10-31 03:13:54 -0600

    Add mongo-config container to docker-compose.yml
    The mongo-config container will automatically provision our mongodb
    instance with a 'test' collection and a 'testUser' who has readWrite
    permissions on that collection.

---
+++ docker/mongo/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+sleep 10
+
+mongo --port 27017 \
+    -u root \
+    -p 'example' \
+    --authenticationDatabase 'admin' \
+    --host mongo.localhost \
+    --eval  "db.createCollection('test'); db.getSiblingDB('test').createUser({user:'testUser', pwd:'testPassword', roles:[{role:'readWrite',db:'test'}]});"
