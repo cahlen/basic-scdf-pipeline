b0bc70c36412876287a75655af10ec4959632194
Cahlen Humphreys <cahlen@gmail.com>
2018-11-04 01:43:44 -0600

    Add initial rest api project skeleton
    This is the intial gradle project skeleton provide by Spring Intializr.
    This project is vanilla, completely untouched. It include mongodb,
    embeded mongo, and web-starter.

---
+++ api/pipeline-rest-api/src/main/java/com/example/restapi/pipelinerestapi/PipelineRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.restapi.pipelinerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineRestApiApplication.class, args);
+    }
+}
