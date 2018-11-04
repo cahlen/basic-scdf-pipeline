9d8e7e54c8f746683a6639d9838fe60289678a9a
Cahlen Humphreys <cahlen@gmail.com>
2018-11-04 04:05:59 -0700

    Update application context with Mock mongo beans

--- api/pipeline-rest-api/src/test/java/com/example/restapi/pipelinerestapi/PipelineRestApiApplicationTests.java
+++ api/pipeline-rest-api/src/test/java/com/example/restapi/pipelinerestapi/PipelineRestApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.restapi.pipelinerestapi;
 
+import com.mongodb.MongoClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PipelineRestApiApplicationTests {
+
+    @MockBean
+    private MongoClient mongoClient;
+
+    @MockBean
+    private MongoDbFactory mongoDbFactory;
 
     @Test
     public void contextLoads() {
